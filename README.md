dd87c82e83bdbe1bff16d799d2c7ead7b53af2c1
Tianyue-Zhao <tiz001@ucsd.edu>
2021-10-31 21:45:05 -0700

    Changed recipes and added URL

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Lab 6 - Starter
+Published website: https://tianyue-zhao.github.io/Lab6_Starter/
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,8 +7,8 @@ const recipes = [
   'https://introweb.tech/assets/json/birthdayCake.json',
   'https://introweb.tech/assets/json/chocolateChip.json',
   'assets/recipes/curry.json',
-  'assets/recipes/pumpkin.json',
-  'assets/recipes/soup.json'
+  'assets/recipes/salad.json',
+  'assets/recipes/pasta.json'
 ];
 
 // Once all of the recipes that were specified above have been fetched, their
